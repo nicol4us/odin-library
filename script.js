6bce122375f3c56fac00258fbaeafd3f77cb55ee
Nicolaus Dwi Noviantono <94541419+nicol4us@users.noreply.github.com>
2025-04-08 11:59:48 +0700

    Add constant declarations

---
+++ script.js
@@ -0,0 +1,9 @@
+
+// Constant Declarations
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+const addNewBookButton = document.querySelector("#addBookButton");
+const dialogToAddNewBook = document.querySelector(".dialogNewBook");
+const submitButton = document.querySelector("#submitButton");
+const myLibrary = Array.from(document.querySelectorAll(".book-container")).reverse();
+const bookContainerWidth = document.querySelector(".shelf").clientWidth;
+const bookWidth = 80;
