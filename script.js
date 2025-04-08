94215eecb75845a33caad9d60f10a58b28ddbb3a
Nicolaus Dwi Noviantono <94541419+nicol4us@users.noreply.github.com>
2025-04-08 12:01:39 +0700

    Add createElement function

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ const submitButton = document.querySelector("#submitButton");
 const myLibrary = Array.from(document.querySelectorAll(".book-container")).reverse();
 const bookContainerWidth = document.querySelector(".shelf").clientWidth;
 const bookWidth = 80;
+
+
+// Function decralations
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// (String, String) -> Element
+// To produce element with class 
+function createElement(container, className) {
+    const part = document.createElement(container);
+    part.classList.add(className);  
+    return part;  
+}
+
