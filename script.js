f5b9f282915e08a0fe0954fd09ff8af6a0b728d4
Nicolaus Dwi Noviantono <94541419+nicol4us@users.noreply.github.com>
2025-04-11 09:40:13 +0700

    Resize book width in constructor

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ function Book(title, author, pages, color, status) {
     this.pages = pages;
     this.color = color
     this.status = status;
-    this.width = 85;    
+    this.width  = 100;      
     this.part = {  
         class: "book",      
         container: "div",
@@ -113,9 +113,8 @@ function Book(title, author, pages, color, status) {
 
 // (Book, ArrayOfBookContainer) -> ()
 // To add Book into an array of container element
-function addBookToShelf(book, library , containerWidth) {
-    const maxBookPerShelf = Math.floor(containerWidth/book.width); 
-    console.log(maxBookPerShelf)      
+function addBookToShelf(book, library , containerWidth) {    
+    const maxBookPerShelf = Math.floor(containerWidth/book.width);         
     library.forEach(container => {
         if(container.childElementCount <= maxBookPerShelf) {
             container.appendChild(book.element);
